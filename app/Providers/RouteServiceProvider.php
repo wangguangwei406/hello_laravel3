7a3b92b29e3549e1163f4cf8810f03432c54f7b5
paul <690438605@qq.com>
2022-08-12 10:01:44 +0800

    基础页面结构

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -18,6 +18,7 @@ class RouteServiceProvider extends ServiceProvider
      * @var string
      */
     public const HOME = '/home';
+    protected  $namespace =  'App\\Http\\Controllers'; // 添加这一行
 
     /**
      * Define your route model bindings, pattern filters, and other route configuration.
@@ -34,7 +35,8 @@ class RouteServiceProvider extends ServiceProvider
                 ->group(base_path('routes/api.php'));
 
             Route::middleware('web')
-                ->group(base_path('routes/web.php'));
+            ->namespace($this->namespace)
+            ->group(base_path('routes/web.php'));
         });
     }
 
