6a6c317ce5bc68636eb7b0a7fa2c90ca7a4e1a31
paul <690438605@qq.com>
2022-08-12 19:00:14 +0800

    话题排序

--- resources/views/topics/index.blade.php
+++ resources/views/topics/index.blade.php
@@ -5,12 +5,28 @@
 @section('content')
 
 <div class="row mb-5">
+
   <div class="col-lg-9 col-md-9 topic-list">
     @if (isset($category))
       <div class="alert alert-info" role="alert">
         {{ $category->name }} ：{{ $category->description }}
       </div>
     @endif
+    <div class="card ">
+      <div class="card-header bg-transparent">
+        <ul class="nav nav-pills">
+          <li class="nav-item">
+            <a class="nav-link {{ active_class( ! if_query('order', 'recent')) }}" href="{{ Request::url() }}?order=default">
+              最后回复
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link {{ active_class(if_query('order', 'recent')) }}" href="{{ Request::url() }}?order=recent">
+              最新发布
+            </a>
+          </li>
+        </ul>
+      </div>
 
       <div class="card-body">
         {{-- 话题列表 --}}
