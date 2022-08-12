be37703db1fda750c0a89a1cd8fa425b2b8b3a73
paul <690438605@qq.com>
2022-08-12 17:37:02 +0800

    生成话题骨架

--- routes/web.php
+++ routes/web.php
@@ -28,3 +28,5 @@ Route::get('email/verify', 'Auth\VerificationController@show')->name('verificati
 Route::get('email/verify/{id}/{hash}', 'Auth\VerificationController@verify')->name('verification.verify');
 Route::post('email/resend', 'Auth\VerificationController@resend')->name('verification.resend');
 Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
+
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
