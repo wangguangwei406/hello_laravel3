231a88f65d90e2ee2d32ece1ca9c9d569e8a1920
paul <690438605@qq.com>
2022-08-14 17:25:49 +0800

    消息通知有新回复

--- app/Http/Requests/ReplyRequest.php
+++ app/Http/Requests/ReplyRequest.php
@@ -6,36 +6,40 @@ class ReplyRequest extends Request
 {
     public function rules()
     {
-        switch($this->method())
-        {
-            // CREATE
-            case 'POST':
-            {
-                return [
-                    // CREATE ROLES
-                ];
-            }
-            // UPDATE
-            case 'PUT':
-            case 'PATCH':
-            {
-                return [
-                    // UPDATE ROLES
-                ];
-            }
-            case 'GET':
-            case 'DELETE':
-            default:
-            {
-                return [];
-            }
-        }
+        // switch($this->method())
+        // {
+        //     // CREATE
+        //     case 'POST':
+        //     {
+        //         return [
+        //             // CREATE ROLES
+        //         ];
+        //     }
+        //     // UPDATE
+        //     case 'PUT':
+        //     case 'PATCH':
+        //     {
+        //         return [
+        //             // UPDATE ROLES
+        //         ];
+        //     }
+        //     case 'GET':
+        //     case 'DELETE':
+        //     default:
+        //     {
+        //         return [];
+        //     }
+        // }
+        return [
+            'content' => 'required|min:2',
+        ];
     }
 
     public function messages()
     {
         return [
-            // Validation messages
+            'content.required' => '内容不能为空',
+            'content.min' => '内容必须至少两个字符',
         ];
     }
 }
