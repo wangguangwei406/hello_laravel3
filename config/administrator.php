7d3d7d6c3ec5582c14a8f2ff4b14e67c2b919ca3
paul <690438605@qq.com>
2022-08-15 18:56:34 +0800

    内容管理后台

--- config/administrator.php
+++ config/administrator.php
@@ -182,6 +182,11 @@ return array(
             'roles',
             'permissions',
         ],
+        '内容管理' => [
+            'categories',
+            'topics',
+            'replies',
+        ],
     ],
 
     /*
