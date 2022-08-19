e27530c7fee4d671d51784e40d364b90a9f61287
paul <690438605@qq.com>
2022-08-19 11:54:27 +0800

    用户最后活跃时间

--- app/Models/User.php
+++ app/Models/User.php
@@ -17,6 +17,7 @@ class User extends Authenticatable implements MustVerifyEmail
     use HasApiTokens, HasFactory, Notifiable, MustVerifyEmailTrait;
     use HasRoles;
     use Traits\ActiveUserHelper;
+    use Traits\LastActivedAtHelper;
 
     use Notifiable {
         notify as protected laravelNotify;
